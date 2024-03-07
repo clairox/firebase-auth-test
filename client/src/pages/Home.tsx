cb0bb51c1f9adc0b83c3437804a01e2d563f0dad
Claire Q <claireq620@gmail.com>
2024-03-07 17:27:57 -0500

    feat: create Home page

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,43 @@
+import LoginButton from '../components/LoginButton'
+import SignupButton from '../components/SignupButton'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from '../lib/firebase'
+import { User } from 'firebase/auth'
+import LogoutButton from '../components/LogoutButton'
+
+const Home = () => {
+	const [user, loading, error] = useAuthState(auth)
+
+	const shownUserProperties: (keyof User)[] = ['uid', 'displayName', 'email', 'emailVerified']
+
+	return (
+		<div>
+			{(loading && <div>Loading...</div>) ||
+				(user ? (
+					<div>
+						{Object.keys(user).map(key => {
+							if (shownUserProperties.includes(key as keyof User)) {
+								return (
+									<p key={key}>
+										{key}: {Object(user)[key] || 'None'}
+									</p>
+								)
+							}
+						})}
+						<LogoutButton />
+					</div>
+				) : (
+					<div>
+						<h1>Welcome to Firebase Auth Test</h1>
+						<p>
+							Click the <strong>Sign Up</strong> button to create a new account or the <strong>Log In</strong> button to log into an existing one.
+						</p>
+						<SignupButton />
+						<LoginButton />
+					</div>
+				))}
+		</div>
+	)
+}
+
+export default Home

83d801abbaeaf176bce6dd47db78da677904a180
Claire Q <claireq620@gmail.com>
2024-03-08 09:46:48 -0500

    feat: convert Input component to ComponentWithAsWithRef type

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -1,11 +1,7 @@
-import { ComponentWithAs, InputProps, Input as Ipt } from '@chakra-ui/react'
-import { RefCallBack } from 'react-hook-form'
+import { InputProps, Input as Ipt } from '@chakra-ui/react'
+import { ComponentWithAsWithRef } from '../types/types'
 
-type Props = InputProps & {
-	innerref: RefCallBack
-}
-
-const Input: ComponentWithAs<'input', Props> = props => {
+const Input: ComponentWithAsWithRef<'input', InputProps> = props => {
 	const { innerref, ...rest } = props
 	return <Ipt {...rest} ref={innerref} variant="filled" bg="blue.900" borderWidth="thin" borderColor="blue.100" focusBorderColor="blue.300" _hover={{ bg: 'blue.1000' }} _focus={{ bg: 'blue.1000' }} />
 }
---
+++ client/src/types/types.d.ts
@@ -0,0 +1,4 @@
+import { As, ComponentWithAs } from '@chakra-ui/react'
+import { RefCallBack } from 'react-hook-form'
+
+interface ComponentWithAsWithRef<Component extends As, Props extends object> extends ComponentWithAs<Component, Props & { innerref: RefCallBack }> {}
